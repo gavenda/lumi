868c41194e7b373d71fd2dd9c0d6e2aaec801b39
Gavenda <gavenda@disroot.org>
2024-02-16 20:23:49 +0800

    Timeout user for mentioning dota 10 times

--- src/app.ts
+++ src/app.ts
@@ -27,11 +27,28 @@ client.once(Events.ClientReady, (readyClient) => {
 });
 
 client.on(Events.MessageCreate, async (message) => {
+  if (!message.channel.isTextBased()) return;
+
   const userId = message.member.id;
   const dota2Key = `dota2:${userId}`;
 
   if (message.cleanContent.toLowerCase().includes('dota')) {
     await redis.incr(dota2Key);
+  }
+  const count = Number(await redis.get(dota2Key));
+
+  if (count % 10 === 0) {
+    if (count === 10) {
+      await message.channel.send(
+        `<@${message.member.id}> has mentioned \`Dota\` at least \`10\` times and has been timed out for a minute.`,
+      );
+      await message.member.timeout(60 * 1000, 'Mentioned dota at least 10 times');
+    } else {
+      await message.channel.send(
+        `<@${message.member.id}> has mentioned \`Dota\` for another \`10\` times, totaling \`${count}\` and has been timed out for a minute.`,
+      );
+      await message.member.timeout(60 * 1000, 'Mentioned dota for another 10 times');
+    }
   }
 });
 
