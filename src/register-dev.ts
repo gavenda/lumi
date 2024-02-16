50e152c2e308033825cf48fbfc67f7139acc0cf7
Gavenda <gavenda@disroot.org>
2024-02-17 01:39:46 +0800

    chore: Add lint, clean, build scripts

--- src/register-dev.ts
+++ src/register-dev.ts
@@ -11,14 +11,15 @@ const rest = new REST().setToken(process.env.TOKEN);
 
 try {
   const clientId = process.env.CLIENT_ID;
+  const guildId = process.env.GUILD_ID;
 
   const commandList = commands.map((command) => command.data.toJSON());
 
-  logger.info(`Started refreshing ${commandList.length} application (/) commands.`);
+  logger.info(`Started refreshing ${commandList.length} application (/) commands on guild id: ${guildId}.`);
 
-  await rest.put(Routes.applicationCommands(clientId), { body: commandList });
+  await rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commandList });
 
-  logger.info(`Successfully reloaded application (/) commands.`);
+  logger.info(`Successfully reloaded application (/) commands on guild id: ${guildId}.`);
 } catch (error) {
   logger.error(error);
 }
