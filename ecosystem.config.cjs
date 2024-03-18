e948131017fc6044e356101f79c196103d080415
Enda <gavenda@disroot.org>
2024-03-18 16:39:26 +0800

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,6 +3,22 @@ const postDeployCommands = {
   development: ['pnpm install', 'pnpm run register:guild', 'pm2 startOrRestart ecosystem.config.cjs', 'pm2 save']
 };
 
+const deploymentConfig = (environment, commands) => {
+  return {
+    'user': process.env.APP_USER,
+    'host': process.env.APP_HOST,
+    'key': 'deploy.key',
+    'ref': process.env.APP_BRANCH,
+    'repo': 'https://github.com/gavenda/lumi',
+    'path': process.env.APP_PATH,
+    'post-deploy': commands,
+    'env': {
+      APP_ENV: environment,
+      DOTENV_KEY: process.env.DOTENV_KEY
+    }
+  };
+};
+
 module.exports = {
   apps: [
     {
