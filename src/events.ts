d94d7622c0722bb66fb5c6ea746c7ba328865ad1
Enda <gavenda@disroot.org>
2024-04-18 03:09:53 +0800

    Temporary turn off dota event

--- src/events.ts
+++ src/events.ts
@@ -2,10 +2,10 @@ import { buttonInteraction } from './events/button-interaction.event';
 import { chatInputCommandInteraction } from './events/chat-input-command-interaction.event';
 import { guildMemberAdd } from './events/guild-member-add.event';
 import { guildMemberRemove } from './events/guild-member-remove.event';
-import { messageCreateDota } from './events/message-create-dota.event';
+// import { messageCreateDota } from './events/message-create-dota.event';
 
 export const events = [
-  messageCreateDota,
+  // messageCreateDota,
   guildMemberRemove,
   guildMemberAdd,
   chatInputCommandInteraction,
